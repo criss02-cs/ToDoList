9e1b6f047b52051d384faa7746456f502d5c4d25
Cristiano Aloigi <cristianoaloigi0@gmail.com>
2023-09-26 09:19:05 +0200

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by GBdev on 26/09/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
